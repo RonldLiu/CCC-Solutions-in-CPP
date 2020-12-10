a3ab8bcffbfa8efe2441dde68866bca652ed7f66
ronaldliu <ronaldliubc@gmail.com>
2020-12-10 11:04:07 -0800

    commit

---
+++ cmake-build-debug/CMakeFiles/19s2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "19s2"
+  "19s2.pdb"
+  "CMakeFiles/19s2.dir/19/19s2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/19s2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
